a5ec2eaa0f6cd3707aed9834f5f20c6b971f4f96
limeen298 <rouinvy.meline@gmail.com>
2026-04-16 01:46:34 +0200

    fix API routes

--- client/v2/portfolio.js
+++ client/v2/portfolio.js
@@ -59,7 +59,7 @@ const setCurrentDeals = ({result, meta}) => {
 const fetchDeals = async (page = 1, size = 6) => {
   try {
     const response = await fetch(
-      `https://lego-server-3ua1gwde9-limeen298s-projects.vercel.app`
+      `https://lego-server-3ua1gwde9-limeen298s-projects.vercel.app/deals?page=${page}&size=${size}`
     );
     const body = await response.json();
 
@@ -78,7 +78,7 @@ const fetchDeals = async (page = 1, size = 6) => {
 const fetchSales = async (id) => {
   try {
     const response = await fetch(
-      `https://lego-server-3ua1gwde9-limeen298s-projects.vercel.app`
+      `https://lego-server-3ua1gwde9-limeen298s-projects.vercel.app/sales?id=${id}`
     );
     const body = await response.json();
 
