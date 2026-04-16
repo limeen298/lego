203286c20954095f6704ffa9c44000f2f7d7a1e3
limeen298 <rouinvy.meline@gmail.com>
2026-04-16 02:20:32 +0200

    modif

--- server/api/deals.js
+++ server/api/deals.js
@@ -1,9 +1,25 @@
+// server/api/deals.js
+import scraper from '../index.js';
+
 export default async function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    data: {
-      result: [],
-      meta: {}
-    }
-  });
+  try {
+    const deals = await scraper(
+      "https://www.avenuedelabrique.com/promotions-et-bons-plans-lego"
+    );
+
+    res.status(200).json({
+      success: true,
+      data: {
+        result: deals,
+        meta: {
+          count: deals.length,
+          currentPage: 1,
+          pageCount: 1
+        }
+      }
+    });
+
+  } catch (e) {
+    res.status(500).json({ success: false, error: e.message });
+  }
 }
