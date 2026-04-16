b337f52e9da581a4f0a87be4ab4ccfa50bf3e45f
limeen298 <rouinvy.meline@gmail.com>
2026-04-16 02:17:24 +0200

    remove vercel config

--- server/api/deals.js
+++ server/api/deals.js
@@ -1,27 +1,9 @@
-import scraper from '../index.js';
-
 export default async function handler(req, res) {
-  try {
-    const data = await scraper(
-      'https://www.avenuedelabrique.com/promotions-et-bons-plans-lego'
-    );
-
-    res.status(200).json({
-      success: true,
-      data: {
-        result: data,
-        meta: {
-          currentPage: 1,
-          pageCount: 1,
-          pageSize: data.length,
-          count: data.length
-        }
-      }
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      error: error.message
-    });
-  }
+  res.status(200).json({
+    success: true,
+    data: {
+      result: [],
+      meta: {}
+    }
+  });
 }
